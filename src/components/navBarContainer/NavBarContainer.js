04f4f63ebe8e482a7240599595c8501146e3e54f
Jacob Hocker <jacobdouglas06@gmail.com>
2023-01-30 13:54:19 -0600

    created the select avatar modal as well as making the login and modal responsive. Display user in nav section

--- src/components/navBarContainer/NavBarContainer.js
+++ src/components/navBarContainer/NavBarContainer.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState, useContext }from 'react';
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import './NavBarContainer.scss';
+import {AuthContext} from '../../Helpers/AuthContext';
+import axios from 'axios';
 import navLogo from '../../assets/images/quiz-champ-sm.png';
 
 export default function NavBarContainer() {
+    const { userId, setAuthState } = useContext(AuthContext);
+    const [userInfo, setUserInfo] = useState({});
 
+    useEffect(() => {
+        axios.get(`http://localhost:2000/auth/userInfo/${userId}`)
+        .then((response) => {
+            setUserInfo(response)
+        })
+    }, [userId])
+
+    const logout = () => {
+        localStorage.removeItem("accessToken")
+        setAuthState({ username: "", id: 0, status: false })
+    };
     return (
         <Navbar collapseOnSelect className='nav-container' expand="sm" >
             <Container>
@@ -23,9 +38,11 @@ export default function NavBarContainer() {
                                 <h2 className='nav-link'>Admin</h2>
                             </Nav.Link>
                             <Nav.Link href='/profile'>
-                                <h2 className='nav-link'>Username</h2>
+                                {userInfo.data && 
+                                    <img src={userInfo.data.avatar} className='nav-avatar' alt={userInfo.data.username} />
+                                }
                             </Nav.Link>
-                            <button>Logout</button>
+                            <button onClick={logout}>Logout</button>
                         </Nav>
                     </div>
                 </Navbar.Collapse>
---
+++ src/components/userRegistration/AvatarModal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './AvatarModal.scss';
+
+
+export default function AvatarModal({ avatarName, setAvatarName, avatarList, avatar, setShowModal, setAvatar }) {
+    
+
+    
+    
+    
+    return (
+        <div className='avatar-modal'>
+            <div className='avatar-modal-content'>
+                <div className='avatar-modal-header'>
+                    <h1 className='avatar-modal-title'>
+                        {avatarName === "" ? 'Choose Avatar' : `${avatarName} selected!`}
+                    </h1>
+                </div>
+                <div className='avatar-modal-body'>
+                    {avatarList.map((avi) => (
+                        <img 
+                        key={avi.id} 
+                        src={avi.avatarAddress} 
+                        alt={avi.avatarName} 
+                        className={avatar === avi.avatarAddress ? 'selected-reg-avatar' : 'reg-avatar'}
+                        onClick={() => {
+                            setAvatar(avi.avatarAddress)
+                            setAvatarName(avi.avatarName)
+                        }}
+                        />
+                    ))}
+                </div>
+                <div className='avatar-modal-footer'>
+                    <button className='auth-btn' onClick={() => {setShowModal(false)}}>Close</button>
+                </div>
+            </div>
+        </div>
+    )
+}
