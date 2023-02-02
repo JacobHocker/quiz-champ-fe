b8edf20cc01419dad797d0c659ae80c514ab8ccb
Jacob Hocker <jacobdouglas06@gmail.com>
2023-02-02 13:44:27 -0600

    finalized the quiz end & quiz results pages.  Styled and made responsive.

--- src/components/quizEnd/QuizEnd.js
+++ src/components/quizEnd/QuizEnd.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Helpers/AuthContext';
+import axios from 'axios';
+import './QuizEnd.scss';
+import { QuizContext } from '../../Helpers/Contexts';
+import QuizResults from '../quizResults/QuizResults';
 
-export default function QuizEnd() {
+export default function QuizEnd({ questionList, quizId }) {
+    const [showResults, setShowResults] = useState(false);
+    const [crownAmount, setCrownAmount] = useState(0);
+
+    const { userId } = useContext(AuthContext);
+    const {  quizScore, setQuizScore, correctAnswers } = useContext(QuizContext);
+
+    useEffect(() => {
+        setQuizScore(Math.round((correctAnswers / questionList.length) * 100))
+
+        if(quizScore >= 60 && quizScore <= 69) {
+            setCrownAmount(1)
+        } else if (quizScore >= 70 && quizScore <= 79) {
+            setCrownAmount(2)
+        } else if (quizScore >= 80 && quizScore <= 89) {
+            setCrownAmount(3)
+        } else if (quizScore >= 90 && quizScore <= 99) {
+            setCrownAmount(4)
+        } else if (quizScore === 100) {
+            setCrownAmount(5)
+        } else {
+            setCrownAmount(0)
+        }
+        
+    }, [quizScore])
+
+    const addScore = () => {
+        axios.post("http://localhost:2000/scores", {
+            quizScore: quizScore,
+            crownAmount: crownAmount,
+            QuizId: quizId,
+            UserId: userId
+        })
+        .then(() => { 
+            setShowResults(true) 
+            
+        })
+    }
     return (
-        <div>QuizEnd</div>
+        <div className='quiz-end-container'>
+            { showResults === false ?
+            <div className='quiz-end'>
+                <h1>Quiz Completed</h1>
+                <button className='neon-purple-btn' onClick={addScore}>
+                    See Results
+                </button>
+            </div>
+            :
+            <QuizResults quizId={quizId} crownAmount={crownAmount} setCrownAmount={setCrownAmount}/>
+            }
+        </div>
     )
 }
--- src/components/quizResults/QuizResults.js
+++ src/components/quizResults/QuizResults.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { QuizContext } from '../../Helpers/Contexts';
+import { AuthContext } from '../../Helpers/AuthContext';
+import './QuizResults.scss';
+import axios from 'axios';
 
-export default function QuizResults() {
+export default function QuizResults({  crownAmount, setCrownAmount }) {
+    const [secondAttemptCrowns, setSecondAttemptCrowns] = useState(0);
+    let navigate = useNavigate();
+    const { quizScore, scoreArr} = useContext(QuizContext);
+    const { userId, userObj } = useContext(AuthContext);
+
+    useEffect(() => {
+        if(scoreArr.data.length === 1) {
+            if(scoreArr.data[0].crownAmount >= crownAmount) {
+                setCrownAmount(0)
+            } else if (scoreArr.data[0].crownAmount < crownAmount) {
+                setSecondAttemptCrowns(crownAmount - scoreArr.data[0].crownAmount)
+            }
+        }
+        
+    }, [scoreArr.data])
+    
+    const saveAndReturn = () => {
+        let totalCrown = userObj.data.totalCrown + crownAmount
+        let totalCrownTwo = userObj.data.totalCrown + secondAttemptCrowns
+
+        if (scoreArr.data.length === 0) {
+            axios.put("http://localhost:2000/auth/crowns", 
+            {totalCrown: totalCrown, id: userId})
+            .then(() => {
+                navigate('/quizzes')
+            })
+        } else if (scoreArr.data.length === 1) {
+            axios.put("http://localhost:2000/auth/crowns", 
+            {totalCrown: totalCrownTwo, id: userId})
+            .then(() => {
+                navigate('/quizzes')
+            })
+        }
+    }
+    
     return (
-        <div>QuizResults</div>
+        <div className='quiz-results-container'>
+            <div className='quiz-end'>
+                <h1>Quiz Completed</h1>
+            </div>
+            <div className='quiz-results'>
+                <h1>Score</h1>
+                <h2> {quizScore}%</h2>
+            </div>
+            <div className='quiz-results'>
+                <h1>Crowns Earned</h1>
+                <h2> {crownAmount}</h2>
+            </div>
+            <div className='return-to-quiz-container'>
+                <button className='neon-purple-btn' onClick={saveAndReturn}>
+                    Return to Quizzes
+                </button>
+            </div>
+        </div>
     )
 }
