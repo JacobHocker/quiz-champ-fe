ab1f8e9ed2751764acbc585283c110885366186a
Jacob Hocker <jacobdouglas06@gmail.com>
2023-02-04 14:19:36 -0600

    added the first table of the quiz rewards section

--- src/components/quizRuleContainer/QuizRuleContainer.js
+++ src/components/quizRuleContainer/QuizRuleContainer.js
@@ -2,21 +2,65 @@ import React from 'react';
 import './QuizRuleContainer.scss';
 
 export default function QuizRuleContainer() {
+
     return (
         <div className='quiz-rule-reward-container'>
             <section className='quiz-rule-container'>
                 <div className='quiz-rule-header'>
                     <h1>Rules</h1>
                 </div>
-                <div className='quiz-rules'>
+                <div className='rules-section'>
                     <h2>Quiz Rules</h2>
-                    <ul className='quiz-rule-list'>
+                    <ul className='rule-list'>
                         <li>Total of 2 attempts for each quiz.</li>
                         <li>There are 30 seconds to answer each question.</li>
                         <li>Between both attempts the max amount of earnable crowns is 5.</li>
-                        <li>If a question is not answered before the timer runs out, the question with automatically move to the next question as a no answer.</li>
+                        <li>If timer hits 0 it will automatically move to next question.</li>
                     </ul>
                 </div>
+                <div className='rules-section'>
+                    <h2>Community Rules</h2>
+                    <ul className='rule-list'>
+                        <li>Be kind and courteous to others when posting and commenting.</li>
+                        <li>No self promotions of any kind.</li>
+                        <li>Hate speech of any kind will result in immediate account termination and ban.</li>
+                    </ul>
+                </div>
+            </section>
+            <section className='quiz-reward-container'>
+                <div className='quiz-reward-header'>
+                    <h1>Rewards</h1>
+                </div>
+                <table className='quiz-reward-table'>
+                    <tr>
+                        <th>Score</th>
+                        <th>Crowns</th>
+                    </tr>
+                    <tr>
+                        <td>100%</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>90% - 99%</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>80% - 89%</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>70% - 79%</td>
+                        <td>2</td>
+                    </tr>
+                    <tr>
+                        <td>60% - 69%</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>0% - 59%</td>
+                        <td>0</td>
+                    </tr>
+                </table>
             </section>
         </div>
     )
