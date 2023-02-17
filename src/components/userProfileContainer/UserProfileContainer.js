bf3308e14cd002e7b5545d5cf6d33302a2385aba
Jacob Hocker <jacobdouglas06@gmail.com>
2023-02-17 10:46:44 -0600

    created params to grab user info by username by URL

--- src/components/userProfileContainer/UserProfileContainer.js
+++ src/components/userProfileContainer/UserProfileContainer.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import './UserProfileContainer.scss';
 
 export default function UserProfileContainer() {
+    const [userPageInfo, setPageUserInfo] = useState({});
+
+    const { username } = useParams();
+
+    
+    useEffect(() => {
+        axios.get(`http://localhost:2000/auth/usernameInfo/${username}`)
+        .then((response) => { setPageUserInfo(response)})
+    }, [username])
+
+
     return (
         <div className='user-profile-container'>
-            <h1>Hello</h1>
+            {   userPageInfo.data && 
+                <h1>{userPageInfo.data.username}</h1>}
 
         </div>
     )
