e672cd603b9118aecf91083c1f44b72b5431d66c
Jacob Hocker <jacobdouglas06@gmail.com>
2023-09-15 14:24:54 -0400

    changed the background image gives the site a more modern look.  ajusted the user avi size in nav bar.

--- src/components/home/RandomQuiz.js
+++ src/components/home/RandomQuiz.js
@@ -34,7 +34,7 @@ export default function RandomQuiz() {
     return (
         <section className='random-quiz-container'>
             <div className='random-quiz-header'>
-                <h1>Random Quiz Generator</h1>
+                <h1>Random Quiz</h1>
                 <p>Don't know what quiz to take? Maybe you don't feel like searching for one?</p>
                 <p>Well take a look at the random quiz we have generated for you and test your knowledge!</p>
             </div>
